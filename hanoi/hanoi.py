a9e1cc06df2c0dac1548c6fe089eb6f2b14ad9b3
szymonsli <szymon.sliwinski@onet.com.pl>
2025-05-17 15:22:19 +0200

    Added number of steps

--- hanoi/hanoi.py
+++ hanoi/hanoi.py
@@ -5,17 +5,20 @@ hanoi = {
   "B": [],
   "C": [],
 }
+steps = 0
 
 
 def build_tower(n, src, via, dest):
+  global steps
   if n > 0:
     # build tower on via
     build_tower(n - 1, src, dest, via)
 
-    # move the last piece from src to dest
+    # move the last disk from src to dest
+    steps += 1
     popped = hanoi[src].pop(0)
     hanoi[dest].insert(0, popped)
-    print(f'Moved plate {popped} from {src} to {dest}')
+    print(f'{steps}. Moved disk {popped} from {src} to {dest}')
     print(hanoi)
 
     # build tower on dest
@@ -23,16 +26,16 @@ def build_tower(n, src, via, dest):
 
 
 def main():
-  arg = ""
+  disks = 0
   try:
-    arg = sys.argv[1]
-    arg = int(arg)
-    if arg < 1:
+    disks = sys.argv[1]
+    disks = int(disks)
+    if disks < 1:
       raise Exception()
-    hanoi["A"] = list(range(1, arg + 1))
+    hanoi["A"] = list(range(1, disks + 1))
   except:
     print("Arg should be an integer greater than 0")
-  build_tower(arg, "A", "B", "C")
+  build_tower(disks, "A", "B", "C")
 
 
 if __name__ == "__main__":
